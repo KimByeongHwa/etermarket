bb75bbb02b27db43814c506ad859b569dfbc209f
KimByeongHwa <iambyeonghwa@gmail.com>
2024-01-14 20:48:21 +0900

    chore: tailwind css, shadcn ui 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-function App() {
-	return <>이터마켓</>;
-}
+import '@/global.css';
+import { Button } from '@/components/ui/button';
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <Button>버튼</Button>
+      <Button className='bg-indigo-500'>버튼</Button>
+      <Button className='bg-pink-500'>버튼</Button>
+    </div>
+  );
+}
