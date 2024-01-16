88f33ff6ac048ec0012bb90282c9ac4b8c358bb3
KimByeongHwa <iambyeonghwa@gmail.com>
2024-01-16 20:20:46 +0900

    feat: 헤더 컴포넌트 제작 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import '@/global.css';
-import { Button } from '@/components/ui/button';
+import Header from './components/layout/Header';
+import Home from './pages/Home';
 
 export default function App() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-      <Button>버튼</Button>
-      <Button className='bg-indigo-500'>버튼</Button>
-      <Button className='bg-pink-500'>버튼</Button>
-    </div>
+    <>
+      <Header />
+      <Home />
+    </>
   );
 }
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className='container'>
+      <div className='container w-11/12 flex items-center justify-between'>
+        <div>로고</div>
+        <div className='text-lg font-semibold'>메뉴</div>
+        <div className='text-smH text-muted-foreground'>로그인/회원가입</div>
+      </div>
+    </div>
+  );
+}
