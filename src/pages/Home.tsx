e17d88cf9dac4bd0f75f473550a53298fe24fe02
KimByeongHwa <iambyeonghwa@gmail.com>
2024-03-22 04:55:45 +0900

    feat: 홈 화면 입장하기 버튼_로그인 조건부 기능 삭제

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { useNavigate } from 'react-router-dom';
-import LogInModal from '@/components/auth/LogInModal';
 
 const features = [
   {
@@ -17,8 +16,6 @@ const features = [
 
 export default function Home() {
   const navigate = useNavigate();
-
-  const logInUserData = localStorage.getItem('userData');
 
   return (
     <div className=' mx-auto my-16 max-w-7xl px-6 lg:my-28 lg:px-8'>
@@ -39,24 +36,12 @@ export default function Home() {
         </dl>
       </div>
       <div className='text-center'>
-        {logInUserData ? (
-          <Button
-            onClick={() => navigate('/etermarket/search-item')}
-            className='bg-primary text-primary-foreground h-11 rounded-md px-24 py-2 hover:bg-primary/90 sm:h-12 sm:px-28 sm:text-lg'
-          >
-            입장하기
-          </Button>
-        ) : (
-          <div className='w-fit mx-auto'>
-            <LogInModal
-              trigger={
-                <div className='inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground h-11 px-24 py-2 hover:bg-primary/90 sm:h-12 sm:px-28 sm:text-lg'>
-                  입장하기
-                </div>
-              }
-            />
-          </div>
-        )}
+        <Button
+          onClick={() => navigate('/etermarket/search-item')}
+          className='bg-primary text-primary-foreground h-11 rounded-md px-24 py-2 hover:bg-primary/90 sm:h-12 sm:px-28 sm:text-lg'
+        >
+          입장하기
+        </Button>
       </div>
     </div>
   );
